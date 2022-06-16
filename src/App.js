cc0afa27aa64692b912204ab33ec65d2c5fcd459
Sagar Adhikari <sagradhkr48@gmail.com>
2022-06-16 14:14:52 +0545

    starting new project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Mater" />
-      <Todo text="Explore" />
-    </div>
-  );
+  return;
 }
 
 export default App;
